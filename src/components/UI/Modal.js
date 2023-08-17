f880d4aa1c31523d9bfa12ff0d3a33b408ddc212
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-17 20:26:23 +0530

    Update Modal.js

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
@@ -18,13 +19,13 @@ const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
-    <>
+    <Fragment>
       {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
-    </>
+    </Fragment>
   );
 };
 
